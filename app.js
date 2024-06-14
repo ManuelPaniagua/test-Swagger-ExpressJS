e941325c669ff8312e70ec04718aaabe191896ef
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-14 14:29:02 +0200

    swagger ui

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSdoc from 'swagger-jsdoc';
+
+
 import technologiesRoutes from './technologies/routes/technologies.routes.js'
 
 const app   = express()
 const port = 3001
 
-/* Glocal middlewares */
+// swagger definition
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Technology API',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`,
+            }
+        ]
+    },
+    apis: ['./technologies/routes/*.js'],
+}
+
+/* Global middlewares */
 app.use(cors())
 app.use(express.json())
+app.use(
+    '/api-docs',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJSdoc(swaggerSpec))
+)
+// app.use(express.json())
 
 /* Routes */
 app.use('/technologies', technologiesRoutes)
--- technologies/routes/technologies.routes.js
+++ technologies/routes/technologies.routes.js
@@ -7,8 +7,106 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *     Technology:
+ *      type: object
+ *      properties:
+ *          id:
+ *              type: integer
+ *              description: Technology id
+ *          name:
+ *              type: string
+ *              description: Technology name
+ *      example:
+ *          id: 1
+ *          name: Swagger
+ */
+
+/**
+ * @swagger
+ * /technologies:
+ *  get:
+ *     summary: Get all technologies
+ *     description: Retrieve a list of all technologies
+ *     responses:
+ *      200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Technology'
+ *      500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /technologies/now:
+ *  get:
+ *     summary: Get current date and/or time
+ *     description: Retrieve the current date and/or time based on the provided format
+ *     parameters:
+ *       - in: query
+ *         name: format
+ *         schema:
+ *           type: string
+ *           enum: [time, date, full]
+ *         description: The format of the response
+ *     responses:
+ *      200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 time:
+ *                   type: string
+ *                   description: The current time (if format is 'time')
+ *                 date:
+ *                   type: string
+ *                   description: The current date (if format is 'date')
+ *                 datetime:
+ *                   type: string
+ *                   description: The full date and time (if format is 'full' or any other value)
+ *      500:
+ *         description: Internal Server Error
+ */
+
+/**
+ * @swagger
+ * /technologies:
+ *  post:
+ *     summary: Add a new technology
+ *     description: Add a new technology to the list
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Technology'
+ *     responses:
+ *      200:
+ *         description: Technology added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Technology'
+ *      400:
+ *         description: Nickname is required
+ *      500:
+ *         description: Internal Server Error
+ */
+
 router.get("/", listTechnologies);
+
 router.get("/now", getDate);
+
 router.post("/", addTechnologie);
 
 export default router;
